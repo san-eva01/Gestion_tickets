6dbb25d646dc2a130d656b4f9ec0920175ee1b53
Andres Ibarrola <andres.daniel.or@gmail.com>
2025-05-06 11:52:23 -0600

    agregar paginaprincipal.java

---
+++ backend/src/main/java/com/tickets/back/controller/PaginaPrincipalController.java
@@ -0,0 +1,12 @@
+package com.tickets.back.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaginaPrincipalController {
+    @GetMapping("/")
+    public String inicio() {
+        return "redirect:/index.html";
+    }
+}
